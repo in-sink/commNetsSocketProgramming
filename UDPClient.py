b943200ac21c2d06d98e3f11d61d83522cd5df58
in-sink <1bashkimuseni1@gmail.com>
2025-12-10 13:51:26 -0600

    Created UDPServer to send message.

--- UDPClient.py
+++ UDPClient.py
@@ -1,13 +1,30 @@
-#boilerplate code taken from ch2 lecture slides
-
+#Initial code taken from ch2 slides
 from socket import *
+import random
+import string
 
-serverName = 'hostname'
+#helper function to generate a random 3 letter code to send to the UDP server
+def generateMessage():
+    message = ""
+    for x in range(3):
+        message = message + (random.choice(string.ascii_lowercase))
+    return message
+
+#port information
+serverName = 'localhost'
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input lowercase sentence:')
-clientSocket.sendto(message.encode(), (serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 
-print(modifiedMessage.decode())
-clientSocket.close()
+#helper funciton to send the UDP message
+def sendUDPMessage():
+    clientSocket = socket(AF_INET,SOCK_DGRAM)
+    message = generateMessage()
+    clientSocket.sendto(message.encode(), (serverName,serverPort))
+    clientSocket.close()
+    print(f"Sent {message} to {serverName} at port {serverPort}")
+
+#helper function to recveive the TCP message
+def recvTCPMessage():
+    print(f"Received ")
+
+sendUDPMessage()
+

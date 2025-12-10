2eb90fb645549dfe7c4421d5294115e69ffed92a
in-sink <1bashkimuseni1@gmail.com>
2025-12-10 13:54:06 -0600

    added tcpserver info

--- UDPClientTCPServer.py
+++ UDPClientTCPServer.py
@@ -28,3 +28,22 @@ def recvTCPMessage():
 
 sendUDPMessage()
 
+#TCP Server code
+#boilerplate code from ch2 ppt
+
+from socket import *
+serverPort = 12000
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+
+print('The server is ready to receive')
+
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence.encode())
+
+    connectionSocket.close()
